9b0b95232cc6dc9b703a9a1cdec53182eaaefd3c
Jarvis Johnson Baker <codelife808@gmail.com>
2022-12-08 20:51:49 -0600

    first commit

---
+++ users/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse
+import pytest
+
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
+
+def test_login_user(client, test_user):
+    test_username, test_password = test_user  # this unpacks the tuple
+    login_result = client.login(username=test_username, password=test_password)
+    assert login_result == True
+
+# Create your tests here.
