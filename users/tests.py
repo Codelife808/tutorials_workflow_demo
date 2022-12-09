9e64c131a1465ba31e470dd6ca7714cd2757c095
Codelife808 <109442763+Codelife808@users.noreply.github.com>
2022-12-09 13:33:55 -0600

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -14,16 +14,19 @@ def another_tutorial(db):
 
 def test_compare_tutorials(new_tutorial, another_tutorial):
     assert new_tutorial.pk != another_tutorial.pk
+    
+@pytest.fixture
+def test_user(db, django_user_model):
+    django_user_model.objects.create_user(
+        username="test_username", password="test_password")
+    return "test_username", "test_password"   # this returns a tuple #Jarvis you added this last and commited documented in case it cause any error
+
 
 def test_login_user(client, test_user):
     test_username, test_password = test_user  # this unpacks the tuple
     login_result = client.login(username=test_username, password=test_password)
     assert login_result == True
 
-@pytest.fixture
-def test_user(db, django_user_model):
-    django_user_model.objects.create_user(
-        username="test_username", password="test_password")
-    return "test_username", "test_password"   # this returns a tuple #Jarvis you added this last and commited documented in case it cause any errors
+
 
 # Create your tests here.
