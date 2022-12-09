3fee5a379e1d2f47acfc5a6d3c3fa99964bdfd29
Codelife808 <109442763+Codelife808@users.noreply.github.com>
2022-12-09 13:04:28 -0600

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -20,4 +20,10 @@ def test_login_user(client, test_user):
     login_result = client.login(username=test_username, password=test_password)
     assert login_result == True
 
+@pytest.fixture
+def test_user(db, django_user_model):
+    django_user_model.objects.create_user(
+        username="test_username", password="test_password")
+    return "test_username", "test_password"   # this returns a tuple #Jarvis you added this last and commited documented in case it cause any errors
+
 # Create your tests here.
