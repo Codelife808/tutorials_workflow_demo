ea3aa7c0185b0b8bbaaa9b9e271e09f9a5f3622b
Codelife808 <109442763+Codelife808@users.noreply.github.com>
2022-12-10 08:26:37 -0600

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -10,9 +10,6 @@ def test_user(db, django_user_model):
     return "test_username", "test_password" 
 
 
-def test_compare_tutorials(new_tutorial, another_tutorial):
-    assert new_tutorial.pk != another_tutorial.pk #just a try out jarvis
-
 
 def test_login_user(client, test_user):
     test_username, test_password = test_user  # this unpacks the tuple
