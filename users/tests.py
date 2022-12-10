a02f576a1f73be72b7d72c65026d212c07338d0d
Codelife808 <109442763+Codelife808@users.noreply.github.com>
2022-12-09 19:22:53 -0600

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -2,19 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 import pytest
 
-"""@pytest.fixture
-def another_tutorial(db):
-    tutorial = Tutorial.objects.create(
-        title='More-Pytest',
-        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
-        description='Tutorial on how to apply pytest to a Django application',
-        published=True
-    )
-    return tutorial"""
-
-
-def test_compare_tutorials(new_tutorial, another_tutorial):
-    assert new_tutorial.pk != another_tutorial.pk 
     
 @pytest.fixture
 def test_user(db, django_user_model):
